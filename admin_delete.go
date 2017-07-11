dad871a132737cdb77a4ecdbc8fd78d36b9f39ca
olehgol <olehgol260@gmail.com>
2017-07-11 18:27:18 +0300

    Add admin methods to bulk delete by public_ids

---
+++ admin_delete.go
@@ -0,0 +1,77 @@
+package cloudinary
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+const (
+	maxDeleteItemsPerRequest = 100
+
+	pathUploadedImages = "/resources/image/upload"
+	pathUploadedRaws   = "/resources/raw/upload"
+)
+
+func (s *Service) bulkDeleteUploadedWithInvalidation(resourceType ResourceType, publicIDs []string) error {
+	if len(publicIDs) == 0 {
+		return nil
+	}
+
+	path := pathUploadedImages
+	if resourceType == RawType {
+		path = pathUploadedRaws
+	}
+
+	var (
+		currentDelimiter  = 0
+		previousDelimiter = 0
+		finish            = false
+	)
+	for !finish {
+		// we cannot give all public ids as the input for the query, thus have
+		// to make requests with subsets of public_ids.
+		// The code take a new bunch of ids and guarantee that there is no out of bounds access.
+		currentDelimiter += maxDeleteItemsPerRequest
+		if currentDelimiter > len(publicIDs) {
+			currentDelimiter = len(publicIDs)
+			finish = true // come to the end of the publicIDs list, shouldn't loop again.
+		}
+		idsForQuery := publicIDs[previousDelimiter:currentDelimiter]
+		previousDelimiter = currentDelimiter
+		qs := url.Values{
+			"invalidate":   []string{"true"},
+			"public_ids[]": idsForQuery, // "[]" here is by design.
+		}
+
+		for {
+			req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s%s?%s", s.adminURI, path, qs.Encode()), nil)
+			if err != nil {
+				return err
+			}
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return err
+			}
+			m, err := handleHttpResponse(resp)
+			if err != nil {
+				return err
+			}
+			if e, ok := m["next_cursor"]; ok {
+				qs.Set("next_cursor", e.(string))
+			} else {
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func (s *Service) DeleteUploadedImages(publicIDs []string) error {
+	return s.bulkDeleteUploadedWithInvalidation(ImageType, publicIDs)
+}
+
+func (s *Service) DeleteUploadedRawFiles(publicIDs []string) error {
+	return s.bulkDeleteUploadedWithInvalidation(RawType, publicIDs)
+}
